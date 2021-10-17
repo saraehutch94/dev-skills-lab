733e8024e8330b60ff1bac5a03c24b0a799698f6
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-17 17:32:31 -0400

    add placeholder for where skills will be placed

--- js/script.js
+++ js/script.js
@@ -2,20 +2,19 @@
 
 let $listItem = $(".list-item");
 
-// $(document).ready(function () {
-//     $listItem.remove();
-// });
+$(document).ready(function () {
+    $listItem.remove();
+});
 
-// User Story #2:
+// User Story #2 + #3:
 
 $("#add-skill-btn").on("click", function() {
     let $valOfEntry = $(".input-box").val();
-    let $addedSkill = $(`<li class="list-item"><button class="remove-btn">X</button>${$valOfEntry}</li>`)
+    let $addedSkill = $(`<li class="list-item"><button class="remove-btn">X</button>${$valOfEntry}</li>`);
     $("#list-items").append($addedSkill);
+    $(".add-skills-here").remove();
     $(".input-box").val("");
     $(".remove-btn").on("click", function() {
         $(this).parent().remove();
     });
 });
-
-// User Story #3: above
