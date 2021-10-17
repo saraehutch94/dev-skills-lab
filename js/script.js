282ecc5f42a62b3c032a47abd62478bbb08c9106
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-17 13:40:21 -0400

    created first user story

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// User Story #1:
+
+let $listItem = $(".list-item");
+
+// $(document).ready(function () {
+//     alert("ready!");
+// });
+
+$(document).ready(function () {
+    $listItem.remove();
+});
+
+// User Story #2:
+
+const arrayOfSkills = [];
+
