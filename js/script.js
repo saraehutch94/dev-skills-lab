2d652dcdb882d43907a46c7314b518cea2d9b2a5
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-17 16:21:34 -0400

    created third user story

--- js/script.js
+++ js/script.js
@@ -13,4 +13,9 @@ $("#add-skill-btn").on("click", function() {
     let $addedSkill = $(`<li class="list-item"><button class="remove-btn">X</button>${$valOfEntry}</li>`)
     $("#list-items").append($addedSkill);
     $(".input-box").val("");
-})
+    $(".remove-btn").on("click", function() {
+        $(this).parent().remove();
+    });
+});
+
+// User Story #3: above
